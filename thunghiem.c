79f67b915f007fe54ef500cf104d33c07c78a1de
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-05-25 09:25:38 +0700

    Add files via upload

---
+++ thunghiem.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main () {
+    int arr[5];
+    int i, max, min, total;
+    float avg;
+    for (i = 0; i < 5; i++) {
+        printf("enter num %d : ", i + 1);
+        scanf("%d", &arr[i]);
+    if(i == 0){
+        max = arr[0];
+        min = arr[0];
+    }else {
+        if(arr[i] > max)
+        max = arr[i];
+        if(arr[i] < min)
+        min = arr[i];
+    }
+    }
+    for (i = 0, total = 0, avg = 0; i < 5; i++){
+        total += arr[i];
+        avg =(float)total/i;
+    }
+    printf("max is : %d", max);
+    printf("\nmin is : %d", min);
+    printf("\nAvg is :%.2f",avg);
+}
